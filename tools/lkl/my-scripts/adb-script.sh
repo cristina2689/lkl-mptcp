4888c27421ce3f90db3f9a9689662c75d1601035
Cristina Opriceana <cristina.opriceana@gmail.com>
2017-07-31 16:19:01 +0900

    Some changes for the 32-arm android version

--- tools/lkl/my-scripts/adb-script.sh
+++ tools/lkl/my-scripts/adb-script.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 # This is to be adb pushed on the arm device
 
-export LD_LIBRARY_PATH=/data/local/tmp
-LKL_HIJACK_DEBUG=1 LD_PRELOAD=1liblkl-hijack.so ip link
+export LD_LIBRARY_PATH=/data/local/tmp/lkl
+LKL_HIJACK_DEBUG=1 LD_PRELOAD=liblkl-hijack.so ip link
 
