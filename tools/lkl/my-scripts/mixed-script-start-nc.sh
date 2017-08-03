54cedd7870384f8976d889f8eb0fa710b02d45f7
Cristina Opriceana <cristina.opriceana@gmail.com>
2017-08-03 15:39:02 +0900

    Add the nc android binary used on 32-bit Android

--- tools/lkl/my-scripts/mixed-script-start-nc.sh
+++ tools/lkl/my-scripts/mixed-script-start-nc.sh
@@ -9,4 +9,4 @@ REQ='GET / HTTP/1.1\r\nHost:www.multipath-tcp.org\r\nUser-Agent: curl/7.D52.1\r\
 
 
 LKL_HIJACK_NET_MAC="c4:9a:02:42:a3:7e" LKL_HIJACK_BOOT_CMDLINE="ip=dhcp" LKL_HIJACK_NET_IFTYPE=raw \
-LKL_HIJACK_NET_IFPARAMS=wlan0 echo -en $REQ | /data/local/tmp/busybox-32 nc 130.104.230.45 80
+    LKL_HIJACK_NET_IFPARAMS=wlan0 echo -en $REQ | /data/local/tmp/nc 130.104.230.45 80
