9fff4ba943134bc689b111be05c427e8ace7f8fd
Cristina Opriceana <cristina.opriceana@gmail.com>
2017-08-04 16:09:10 +0900

    Some cleanup on the scripts

--- tools/lkl/my-scripts/mp-nc.sh
+++ tools/lkl/my-scripts/mp-nc.sh
@@ -1,10 +1,12 @@
-#!/system/bin
+#!/system/bin/sh
 
 # >> Android
 #
 # Fake request for the curl User Agent since the MPTCP server
 # is set to respond to it with TRUE/FALSE if you are mptcp capable
 
-echo -en 'GET / HTTP/1.1\r\nHost: www.multipath-tcp.org\r\nUser-Agent: curl/7.D52.1\r\n Accept: */*\r\n\r\n' | nc www.multipath-tcp.org 80
+REQ='GET / HTTP/1.1\r\nHost: www.multipath-tcp.org\r\nUser-Agent: curl/7.D52.1\r\n Accept: */*\r\n\r\n'
 
-# echo -en 'GET / HTTP/1.1\r\nHost: www.multipath-tcp.org\r\nUser-Agent: curl/7.D52.1\r\n Accept: */*\r\n\r\n' | ./busybox-32 nc www.multipath-tcp.org 80
+echo -en $REQ | /data/local/tmp/busybox-32 nc 130.104.230.45 80
+
+# echo -en $REQ | /data/local/tmp/busybox-32 nc www.multipath-tcp.org 80
--- tools/lkl/my-scripts/start-curl.sh
+++ tools/lkl/my-scripts/start-curl.sh
@@ -1,12 +1,19 @@
 #!/system/bin/sh
 
 export LD_LIBRARY_PATH=/data/local/tmp/lkl:/data/local/tmp/curl-arm32/
-export LD_PRELOAD=liblkl-hijack.so
 export LD_HIJACK_DEBUG=1
 export LKL_HIJACK_DEBUG=1
-export LKL_HIJACK_NET_MAC="c4:9a:02:42:a3:7e"
-export LKL_HIJACK_BOOT_CMDLINE="ip=dhcp"
-export LKL_HIJACK_NET_IFTYPE=raw
-export LKL_HIJACK_NET_IFPARAMS=wlan0
+export LKL_HIJACK_NET_IFTYPE0=raw
+export LKL_HIJACK_NET_IFPARAMS0=wlan0
+export LKL_HIJACK_NET_MAC0="c4:9a:02:42:a3:7e"
+
+
+export LKL_HIJACK_NET_IFTYPE1=raw
+export LKL_HIJACK_NET_IFPARAMS1=rmnet0
+# export LKL_HIJACK_NET_MAC1=
+
+
+export LKL_HIJACK_BOOT_CMDLINE="ip=dhcp" 
+export LD_PRELOAD=liblkl-hijack.so
 
 /data/local/tmp/curl-arm32/curl --resolve multipath-tcp.org:80:130.104.230.45  http://multipath-tcp.org
